9b5d7db3df0531c02b2a9961ab56a596c17447fa
Willie Louth <willielouth@gmail.com>
2019-12-16 18:39:07 -0600

    Create modal

---
+++ client/src/components/Modal.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const modal = props => (
+  <div className="modal">
+    <header className="modal__header"><h1>{props.title}</h1></header>
+    <section className="modal__content">
+      {props.children}
+    </section>
+    <section className="modal__actions">
+      {props.canCancel && <button className="btn" onClick={props.onCancel}>Cancel</button> }
+      {/* {props.canConfirm && <button className="btn" onClick={props.onConfirm}>{props.submit}</button> } */}
+    </section>
+  </div>
+);
+
+export default modal;

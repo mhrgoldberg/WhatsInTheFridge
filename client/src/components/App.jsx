03e5657de5470c511a8496ebf27e50f704fab40e
mitchellreiss <mitchellreiss@gmail.com>
2019-12-19 16:31:41 -0800

    Update app.jsx imports

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,20 +3,19 @@ import "../App.css";
 import React from "react";
 // import { Query } from "react-apollo";
 import { Route, Switch } from "react-router-dom";
-import Login from "./Login.jsx";
-
 import Splash from "./Splash.jsx";
-import Register from "./Register.jsx";
 import AuthRoute from "../util/route_util.js";
 import "../stylesheets/main.scss";
-import NutritionPieChart from './nutrition_pie_chart';
-import Search from './Search.jsx';
-import SearchAdvanced from './SearchAdvanced.jsx';
+import NutritionPieChart from "./nutrition_pie_chart";
+import Search from "./Search.jsx";
+import SearchAdvanced from "./SearchAdvanced.jsx";
+import IngredientForm from "./grocery_list/ingredient_form";
+import IngredientSearch from "./fridge/ingredient_search";
 
 const App = () => {
   return (
     <Switch>
-      <Route path="/ingredient" component={NutritionPieChart} />
+      <Route path="/ingredient" component={IngredientSearch} />
       <AuthRoute exact path="/" component={Splash} routeType="auth" />
       <Route exact path="/search" component={Search} />
       <Route exact path="/advanced-search" component={SearchAdvanced} />

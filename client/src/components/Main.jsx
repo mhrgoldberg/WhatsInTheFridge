0dd30caefdb79ece249f5aa3695106c0f0cffa82
mitchellreiss <mitchellreiss@gmail.com>
2019-12-30 00:29:56 -0800

    Link fridge and search

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,26 +1,42 @@
-import React, { Component } from 'react';
-import Nav from './Nav.jsx';
-import Search from './RecipeSearch/Search';
-import Fridge from './fridge/fridge';
+import React, { Component } from "react";
+import Nav from "./Nav.jsx";
+import Search from "./RecipeSearch/Search";
+import Fridge from "./fridge/fridge";
 
 class Main extends Component {
- 
+  constructor(props) {
+    super(props);
+    this.state = {
+      fridgeArr: []
+    };
+    this.addToFridge = this.addToFridge.bind(this);
+  }
+
+  addToFridge(item) {
+    const fridgeArrCopy = [...this.state.fridgeArr];
+    if (item && this.state.fridgeArr.length < 4) {
+      fridgeArrCopy.push(item);
+      this.setState({
+        fridgeArr: fridgeArrCopy
+      });
+    }
+  }
+
   render() {
     return (
       <div className="main-container">
         <Nav />
         <div className="main-outer-container">
           <div className="main-inner-container1">
-            <Fridge />
+            <Fridge fridgeArr={this.state.fridgeArr} addToFridge={this.addToFridge} />
           </div>
           <div className="main-inner-container2">
-            <Search />
+            <Search fridgeArr={this.state.fridgeArr} />
           </div>
-          <div className="main-inner-container3">
-          </div>
+          <div className="main-inner-container3"></div>
         </div>
       </div>
-    )
+    );
   }
 }
 
--- client/src/components/RecipeSearch/SearchRecipes.jsx
+++ client/src/components/RecipeSearch/SearchRecipes.jsx
@@ -5,20 +5,20 @@ const SearchRecipes = props => {
   console.log(props.recipes);
   return <div className="search-recipes-list">
     {props.recipes.map((recipe, i) => {
-      console.log(recipe);
-      console.log(recipe.recipe.label);
-      console.log(recipe.recipe.url);
-      console.log(recipe.recipe.uri);
-      console.log(recipe.recipe.image);
-      console.log(recipe.recipe.ingredientLines);
-      console.log(recipe.recipe.calories);
-      console.log(recipe.recipe.yield);
-      console.log(recipe.recipe.digest[0].total);
-      console.log(recipe.recipe.digest[0].daily);
-      console.log(recipe.recipe.digest[1].total);
-      console.log(recipe.recipe.digest[1].daily);
-      console.log(recipe.recipe.digest[2].total);
-      console.log(recipe.recipe.digest[2].daily);
+      // console.log(recipe);
+      // console.log(recipe.recipe.label);
+      // console.log(recipe.recipe.url);
+      // console.log(recipe.recipe.uri);
+      // console.log(recipe.recipe.image);
+      // console.log(recipe.recipe.ingredientLines);
+      // console.log(recipe.recipe.calories);
+      // console.log(recipe.recipe.yield);
+      // console.log(recipe.recipe.digest[0].total);
+      // console.log(recipe.recipe.digest[0].daily);
+      // console.log(recipe.recipe.digest[1].total);
+      // console.log(recipe.recipe.digest[1].daily);
+      // console.log(recipe.recipe.digest[2].total);
+      // console.log(recipe.recipe.digest[2].daily);
 
       return <SearchRecipeItem key={i} recipe={recipe} currentUserId={props.currentUserId}/>
     })}

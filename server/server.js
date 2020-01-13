84f476baeb14cef466d44ddbe2e3588f8676f3c0
mitchellreiss <mitchellreiss@gmail.com>
2020-01-13 10:32:45 -0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,9 +13,9 @@ if (!db) {
   throw new Error("You must provide a string to connect to MongoDB Atlas");
 }
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
+  app.use(express.static('client/build'));
   app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   })
 }
 

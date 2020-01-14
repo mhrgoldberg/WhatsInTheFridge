a173558cf0a4814ca39f45dd2c8b5f3ba7e3b3b3
mitchellreiss <mitchellreiss@gmail.com>
2020-01-14 15:32:00 -0800

    Update server

--- server/server.js
+++ server/server.js
@@ -12,21 +12,14 @@ const path = require('path');
 if (!db) {
   throw new Error("You must provide a string to connect to MongoDB Atlas");
 }
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  })
-}
+
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.use(bodyParser.json());
 
-app.use(cors());
 
 app.use(
   "/graphql",
@@ -40,5 +33,16 @@ app.use(
     };
   })
 );
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
+
+app.use(bodyParser.json());
+
+app.use(cors());
 
 module.exports = app;

7403b95841086abad92107b9a7b80da1ad23cc65
mitchellreiss <mitchellreiss@gmail.com>
2020-02-11 16:50:51 -0800

    Change loading spinner color

--- client/src/components/loading.jsx
+++ client/src/components/loading.jsx
@@ -3,7 +3,7 @@ import { RotateLoader } from "react-spinners";
 const Loading = () => {
 		return (
 			<div className="loading-spinner">
-				<RotateLoader color={"green"} size={10} />
+				<RotateLoader color={"green"} />
 			</div>
 		);
 };
